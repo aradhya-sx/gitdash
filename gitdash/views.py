212925018f6eabb5d827b711b8396f5eee842d16
Aradhya <aradhya.sx@gmail.com>
2017-09-10 22:02:59 +0530

    changed the quote function

--- gitdash/views.py
+++ gitdash/views.py
@@ -5,9 +5,4 @@ from helper import generate_random_qoute, read_quotes
 @app.route('/')
 @app.route('/home')
 def home():
-    quotes = read_quotes('quotes.txt')
-    authors = list(set(author for _, author in quotes))
-    quote, _ = random.choice(quotes)
-    author = random.choice(authors)
-    print quote.encode('utf-8'), '-', author.encode('utf-8')
     return generate_random_qoute()
