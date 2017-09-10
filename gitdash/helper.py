73f529184a2a0e55c24a022f29ca8dd8fb89d900
Aradhya <aradhya.sx@gmail.com>
2017-09-10 23:28:08 +0530

    unicode esacped

--- gitdash/helper.py
+++ gitdash/helper.py
@@ -1,8 +1,11 @@
 from random import randrange
 from quotes import quotes
+import unicodedata
+
 
 def generate_random_qoute():
     total_quotes = len(quotes)
     random_index = randrange(0, total_quotes)
     quote = quotes[random_index]
-    return quote
+    q = unicodedata.normalize('NFKD', quote).encode('ascii','ignore')
+    return q
